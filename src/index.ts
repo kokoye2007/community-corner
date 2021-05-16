74f28797a6b07c181beca062ed8da73be85fa9ff
sarnold28 <51327430+sarnold28@users.noreply.github.com>
2021-05-16 12:17:51 +0200

    test

--- src/index.ts
+++ src/index.ts
@@ -5,3 +5,22 @@
 
 console.log('Script started successfully');
 WA.openCoWebSite('https://workadventu.re');
+
+let helloWorldPopup;
+
+// Open the popup when we enter a given zone
+helloWorldPopup = WA.onEnterZone('popup', () => {
+    WA.openPopup("popUp", 'Hello world!', [{
+        label: "Close",
+        className: "primary",
+        callback: (popup) => {
+            // Close the popup when the "Close" button is pressed.
+            popup.close();
+        }
+    });
+}]);
+
+// Close the popup when we leave the zone.
+WA.onLeaveZone('popup', () => {
+    helloWorldPopup.close();
+});
